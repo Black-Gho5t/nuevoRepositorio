3f58916cd4cd3fd9197b2bf3c441eb9d491b46a0
Black-Gho5t <yader877584@gmail.com>
2024-10-16 18:12:18 -0500

    nuevo commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavModule } from './nav/nav.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppModule { }
